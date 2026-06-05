bac5db16a9bc227e4d02c552ef36040c41a595ef
Alvaro Calderon <alvarocalderon@kinal.edu.gt>
2026-06-05 10:51:02 -0600

    011 - agreago actualización a Principal.java

--- src/org/alvarocalderon/system/Principal.java
+++ src/org/alvarocalderon/system/Principal.java
@@ -8,7 +8,7 @@ public class Principal {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo - Soy el profesor");
-        System.out.println("Estoy en kinal - 2006576");
+        System.out.println("Estoy en kinal - 2006576-");
     }
     
 }

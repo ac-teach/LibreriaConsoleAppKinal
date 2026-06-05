6e874b0bb73f81353c44881e3349a19945d1ae11
Alvaro Calderon <alvarocalderon@kinal.edu.gt>
2026-06-05 10:44:54 -0600

    010 se actulizo la clase Principal, agregue un saludo

--- src/org/alvarocalderon/system/Principal.java
+++ src/org/alvarocalderon/system/Principal.java
@@ -7,7 +7,8 @@ package org.alvarocalderon.system;
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo - Soy el profesor");
+        System.out.println("Estoy en kinal - 2006576");
     }
     
 }
